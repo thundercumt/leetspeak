63af7cf9a74b2eff856b0d53433636e5ecc2d05c
Thunder Liu <thundercumt@126.com>
2019-10-21 22:28:28 +0100

    89 perfect gray code

--- 89.cpp
+++ 89.cpp
@@ -7,32 +7,10 @@ public:
     vector<int> grayCode(int n) {
         vector<int> v;
         int size = 1 << n;
-        vector<int> s(size, 0);
-        dfs(s, v, size, n, 0);
+        for (int i=0; i<size; ++i) {
+            v.push_back(i ^ (i >> 1));
+        }
         return v;
-    }
-
-private:
-
-    bool dfs(vector<int> &s, vector<int> &v, const int maxSize, const int m, int n) {
-        if (v.size() == maxSize) {
-            return true;
-        }
-        
-        s[n] = 1;
-        v.push_back(n);
-        bool ok = false;
-        for (int i=0; i<m; ++i) {
-            int candidate = n ^ (1 << i);
-            if (s[candidate] == 0) {
-                ok |= dfs(s, v, maxSize, m, candidate);
-            }
-        }
-        if (!ok) {
-            s[n] = 0;
-            s.pop_back();
-        }
-        return ok;
     }
 };
 
