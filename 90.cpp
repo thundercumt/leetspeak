c4221e6ac00bedb9612c0f8ca3a0964f9d3f9ee7
Thunder Liu <thundercumt@126.com>
2019-10-26 18:01:36 +0100

    90 better implementation recursive without set 99.57%

--- 90.cpp
+++ 90.cpp
@@ -4,83 +4,40 @@
 
 using namespace std;
 
-class SolutionOK {
-public:
-    vector< vector<int> > subsetsWithDup(vector<int>& nums) {
-        set< vector<int> > s;
-        vector< vector<int> > v;
-        if (nums.size() == 0) return v;
-        sort(nums.begin(), nums.end());
-        subset(s, v, nums, nums.size());
-        return v;
-    }
-private:
-    void subset(set< vector<int> > &s, vector< vector<int> > &v, vector<int>& nums, int m) {
-        if (m == 0) {
-            vector<int> empty;
-            s.insert(empty);
-            v.push_back(empty);
-        }
-        else {
-            subset(s, v, nums, m-1);
-            int count = v.size();
-            for (int i=0; i<count; ++i) {
-                vector<int> copy(v[i]);
-                copy.push_back(nums[m-1]);
-                if (s.find(copy) == s.end()) {
-                    s.insert(copy);
-                    v.push_back(copy);
-                }
-            }
-        }
-    }
-};
-
-
 class Solution {
 public:
-    vector< vector<int> > subsetsWithoutDup(vector<int>& nums) {
-        vector< vector<int> > v;
-        v.push_back(vector<int>());
-        int size = nums.size();
-        if (size == 0) return v;
-
-        sort(nums.begin(), nums.end());
-        
-        for (int i=0; i<size; ++i) {
-            for (int j=0; j<v.size(); ++j) {
-                vector<int> copy(v[j]);
-                copy.push_back(nums[i]);
-                v.push_back(copy);
-            }
-        }
-        
-        return v;
-    }
-
     vector< vector<int> > subsetsWithDup(vector<int>& nums) {
         vector< vector<int> > v;
-        if (nums.size() == 0) return v;
         sort(nums.begin(), nums.end());
         subset(v, nums, nums.size());
         return v;
     }
 private:
-    void subset(vector< vector<int> > &v, vector<int>& nums, int m) {
+    int subset(vector< vector<int> > &v, vector<int>& nums, int m) {
         if (m == 0) {
             v.push_back(vector<int>());
+            return 1;
         }
         else {
-            subset(v, nums, m-1);
+            int lastAdded = subset(v, nums, m - 1);
             int count = v.size();
-            for (int i=0; i<count; ++i) {
+            int startFrom;
+            if (m == 1 || nums[m-1] != nums[m-2]) {
+                startFrom = 0;
+            } else {
+                startFrom = count - lastAdded;
+            }
+            for (int i=startFrom; i<count; ++i) {
                 vector<int> copy(v[i]);
                 copy.push_back(nums[m-1]);
                 v.push_back(copy);
             }
+            return count - startFrom;
         }
     }
 };
+
+
 
 int main() {
     int x[] = {1,2,2,3,3,3};
