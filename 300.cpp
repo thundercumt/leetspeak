098969078bd96dcf5c54d4e79d3fd4742e7a8798
Thunder Liu <thundercumt@126.com>
2019-10-28 19:33:26 +0000

    300 nlogn 90%

--- 300.cpp
+++ 300.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-class Solution {
+class SolutionOn2 {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int sz = nums.size();
         if (sz == 0) return 0;
         
         vector<int> v(sz, 1);
-        int maxLen = 1;
+        int maxAll = 1;
         for (int i=sz-2; i>=0; --i) {
             int maxCur = v[i];
             for (int j=i+1; j<sz; ++j) {
@@ -18,15 +18,34 @@ public:
                 }
             }
             v[i] = maxCur;
-            maxLen = maxCur > maxLen ? maxCur : maxLen;
+            maxAll = maxCur > maxAll ? maxCur : maxAll;
         }
-        return maxLen;
+        return maxAll;
     }
 };
 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()) 
+            return 0;
+        
+        vector<int> v;
+        int n = nums.size();
+        for (int i=0; i<n; ++i) {
+            vector<int>::iterator it = lower_bound(v.begin(), v.end(), nums[i]);
+            if (it == v.end()) {
+                v.push_back(nums[i]);
+            } else {
+                *it = nums[i];
+            }
+        }
+        return v.size();
+    }
+};
 
 int main() {
-    int a[] = {10,9,2,5,3,7,101,18};
+    int a[] = {2,3,7,1,4,8};
     vector<int> v(a, a + sizeof(a)/sizeof(a[0]));
     int l = Solution().lengthOfLIS(v); 
     printf("%d\n", l);
